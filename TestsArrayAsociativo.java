856d912ec3ffe8b4658da0b5af3f0d64ea6b190c
Pedro Avila <srsworeb@gmail.com>
2017-05-16 17:29:32 +0200

    Version 1.1

--- TestsArrayAsociativo.java
+++ TestsArrayAsociativo.java
@@ -2,14 +2,30 @@ package prArrayAsociativo;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Test;
 
 public class TestsArrayAsociativo {
 
+	ArrayAsociativo aa;
+	
+	@After
+	public void finish(){
+		aa = null;
+	}
+	
 	@Test
 	public void test1() {
-		ArrayAsociativo aa = new ArrayAsociativo();
+		aa = new ArrayAsociativo();
 		assertEquals(new Integer(0), new Integer(aa.size()) );
+	}
+	
+	@Test
+	public void test2(){
+		String[] keys =  {"uno","dos","tres"};
+		String[] values = {"1","2","3"};
+ 		aa = new ArrayAsociativo(keys,values);
+ 		assertEquals(new Integer(keys.length),new Integer(aa.size()));
 	}
 
 }
