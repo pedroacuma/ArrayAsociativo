4bdcc74fa5242ea894911835300dbc6932e2cbd5
pedroacuma <example@outlook.com>
2017-05-16 09:47:01 +0200

    Versión Inicial

---
+++ TestsArrayAsociativo.java
@@ -0,0 +1,14 @@
+package prArrayAsociativo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestsArrayAsociativo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
